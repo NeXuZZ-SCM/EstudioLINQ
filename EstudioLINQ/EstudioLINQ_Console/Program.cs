03ec6a760552b94dfe801d713bd047231e8b9427
nexuzz <nexuzz@outlook.com>
2019-01-18 18:06:01 -0300

    Proceso de filtrado LINQ

--- EstudioLINQ/EstudioLINQ_Console/Program.cs
+++ EstudioLINQ/EstudioLINQ_Console/Program.cs
@@ -8,25 +8,30 @@ namespace EstudioLINQ_Console
 {
     class Program
     {
-        
+        const string MENSAJE_INICIO = "LAB LINQ\n";
         static void Main(string[] args)
         {
-            Console.WriteLine("LAB LINQ");
+            Console.WriteLine(MENSAJE_INICIO);
 
+            #region Listado INT Precios  
             List<int> precios = new List<int>();
             precios.Add(29);
             precios.Add(15);
             precios.Add(20);
             precios.Add(19);
             precios.Add(3);
+            #endregion
 
+            #region Listado INT Cantidad
             List<int> cantidad = new List<int>();
             cantidad.Add(22);
             cantidad.Add(33);
             cantidad.Add(44);
             cantidad.Add(55);
             cantidad.Add(66);
+            #endregion
 
+            #region Listado STRING Productos
             List<string> productos = new List<string>();
             productos.Add("jugo");
             productos.Add("gaseosa");
@@ -35,60 +40,84 @@ namespace EstudioLINQ_Console
             productos.Add("tomate");
             productos.Add("shampoo");
             productos.Add("pan");
-
+            #endregion
 
             PreciosMenorA(precios);
             ListarPrecio(precios);
             SumarPrecio(precios);
             QuitarElementos(precios);
-            UnirPrecioACantidad(precios, cantidad);
+            UnirPrecioACantidad(precios, cantidad); 
+            QueContenganLaLetra(productos); //Proceso de filtrado
 
-            
 
-            //precios.ToList()
-            //precios.
+
             Console.ReadKey();
         }
 
-        private static void UnirPrecioACantidad(List<int> precios, List<int> cantidad)
-        {
-            foreach (var item in precios.Union(cantidad))
-            {
-                Console.WriteLine(item);
-            }
-        }
 
-        private static void QuitarElementos(List<int> precios)
-        {
-            foreach (var item in precios.Skip(3))
-            {
-                Console.WriteLine(item);
-            }
-            Console.WriteLine("fin SKIP precios | Elimina una cantidad de elementos especificados");
-        }
 
-        private static void SumarPrecio(List<int> precios)
+        #region VER METODOS
+
+        private static void PreciosMenorA(List<int> precios)
         {
-            Console.WriteLine(precios.Sum(precio => precio));
-            Console.WriteLine("fin SUM precios");
+            foreach (var item in precios.Where(x => x < 20))
+            {
+                Console.Write("[" + item + "]  ");
+            }
+            Console.WriteLine("\nfin foreach WHERE x=> x < 20");
         }
 
         private static void ListarPrecio(List<int> precios)
         {
             foreach (var item in precios.ToList())
             {
-                Console.WriteLine(item);
+                Console.Write("[" + item + "]  ");
             }
-            Console.WriteLine("fin foreach ToList");
+            Console.WriteLine("\nfin foreach ToList");
         }
 
-        private static void PreciosMenorA(List<int> precios)
+        private static void SumarPrecio(List<int> precios)
         {
-            foreach (var item in precios.Where(x => x < 20))
-            {
-                Console.WriteLine(item);
-            }
-            Console.WriteLine("fin foreach WHERE x=> x < 20");
+            Console.WriteLine("[" + precios.Sum(precio => precio) + "]  ");
+            Console.WriteLine("fin SUM precios");
         }
+
+        private static void QuitarElementos(List<int> precios)
+        {
+            foreach (var item in precios.Skip(3))
+            {
+                Console.Write("[" + item + "]  ");
+            }
+            Console.WriteLine("\nfin SKIP precios | Elimina una cantidad de elementos especificados");
+        }
+
+        private static void UnirPrecioACantidad(List<int> precios, List<int> cantidad)
+        {
+            foreach (var item in precios.Union(cantidad))
+            {
+                Console.Write("[" + item + "]  ");
+            }
+            Console.WriteLine("\nFIN Unir Precio a Cantidad");
+        }
+
+        private static void QueContenganLaLetra(List<string> productos)
+        {
+            foreach (var item in productos.Where(e => e.Contains("a")).ToList())
+            {
+                Console.Write("[" + item + "]  ");
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+        #endregion 
     }
 }
